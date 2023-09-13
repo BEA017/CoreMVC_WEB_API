e0dc435e54723bafc1d3fc76837f114792a57d7e
ПК <bodnya.evgenij98@gmail.com>
2023-09-13 18:04:04 +0300

    Delete obj

--- CoreMVC_WEB_API/Program.cs
+++ CoreMVC_WEB_API/Program.cs
@@ -7,6 +7,7 @@ namespace CoreMVC_WEB_API
     {
         public static void Main(string[] args)
         {
+			//start
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
